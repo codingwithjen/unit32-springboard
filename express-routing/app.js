9a2d9c0d0f4dfdf88e25b06e6eaaa0ac82ecb783
jen f <77820429+jenfausto@users.noreply.github.com>
2022-03-22 21:24:49 -0600

    expressError.js file added; app.js adding mean route

--- express-routing/app.js
+++ express-routing/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 
+app.get('/mean', function(req, res. next) {
+
+})
+
+
 // goes towards the bottom of the file
 app.listen(3000, function () {
     console.log('App on port 3000');
