3e4aeb0fa2e8142fc14b4b94f99d7bec0d98c3f8
jen f <77820429+jenfausto@users.noreply.github.com>
2022-03-22 21:34:53 -0600

    mean route updated, and express json parse imported

--- express-routing/app.js
+++ express-routing/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 
+const ExpressError = require('./expressError');
 
-app.get('/mean', function(req, res. next) {
+// Tell Express to parse request bodies for either form data or JSON:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-})
+// Requirements: all routes accept GET requests
+app.get('/mean', function(res, req, next) {
+    try {
+        const nums = getNums(req.query.nums);
+        const result = getMean(nums);
+
+        return res.json({ operation: 'mean', value: result });
+    } catch (err) {
+        next(err);
+    }
+});
 
 
 // goes towards the bottom of the file
