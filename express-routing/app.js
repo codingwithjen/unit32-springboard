df537059d1f6d20f44f6453b7ed22d1b6dee7424
jen f <77820429+jenfausto@users.noreply.github.com>
2022-03-22 21:49:14 -0600

    updated

--- express-routing/app.js
+++ express-routing/app.js
@@ -1,26 +1,66 @@
 const express = require('express');
 const app = express();
-
 const ExpressError = require('./expressError');
+const { getNums, getMean, getMedian, getMode } = require('./helper');
 
 // Tell Express to parse request bodies for either form data or JSON:
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Requirements: all routes accept GET requests
-app.get('/mean', function(res, req, next) {
+app.get('/mean', function(req, res, next) {
     try {
-        const nums = getNums(req.query.nums);
-        const result = getMean(nums);
+        let nums = getNums(req.query.nums);
+        let results = getMean(nums);
 
-        return res.json({ operation: 'mean', value: result });
+        return res.json({ operation: 'mean', value: results });
     } catch (err) {
         next(err);
     }
 });
 
+app.get('median', function(req, res, next) {
+    try {
+        let nums = getNums(req.query.nums);
+        let results = getMedian(nums);
 
-// goes towards the bottom of the file
+        return res.json({ operation: 'median', value: results });
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.get('/mode', function(req, res, next) {
+    try {
+        let nums = getNums(req.query.nums);
+        let results = getMode(nums);
+
+        return res.json({ operation: 'mode', value: results });
+    } catch(err) {
+        next(err);
+    }
+});
+
+// 4040 Handler
+app.use(function(req, res, next) {
+    const notFoundError = new ExpressError("Not Found", 404);
+    return next(notFoundError)
+});
+
+// Generic Error Handler
+app.use(function(err, req, res, next) {
+    // The default status is 500 Internal Server Error
+    let status = err.status || 500;
+    let message = err.message;
+
+    // Set the status and alert the user
+    return res.status(status).json({
+        error: { message, status }
+    });
+});
+
+
+// Goes towards the bottom of the file -- The Generic Error Handler
 app.listen(3000, function () {
     console.log('App on port 3000');
 })
--- express-routing/helpers.js
+++ express-routing/helpers.js
@@ -1,1 +1,25 @@
 const ExpressError = require('./expressError');
+
+/** Function for checking the numbers and return the Array of numbers
+ * @param {*} numStr
+ */
+
+function getNums(numStr) {
+    if (numStr === undefined || numStr.trim().length === 0) {
+        throw new ExpressError('NUMS ARE REQUIRED!!!', 400);
+    }
+
+    const nums = numStr.split(',');
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        nums[i] = Number(num);
+
+        if (isNaN(nums[i])) {
+            // if not a number, throw an error
+            throw new ExpressError(`${num} is not a number`, 400);
+        }
+    }
+    return nums;
+}
+
