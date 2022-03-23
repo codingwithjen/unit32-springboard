c5e0bc87239464f0a7191f39c0a8e38ed57f5326
jen f <77820429+jenfausto@users.noreply.github.com>
2022-03-22 21:54:09 -0600

    helpers functions added

--- express-routing/helpers.js
+++ express-routing/helpers.js
@@ -9,10 +9,10 @@ function getNums(numStr) {
         throw new ExpressError('NUMS ARE REQUIRED!!!', 400);
     }
 
-    const nums = numStr.split(',');
+    let nums = numStr.split(',');
 
     for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
+        let num = nums[i];
         nums[i] = Number(num);
 
         if (isNaN(nums[i])) {
@@ -22,4 +22,66 @@ function getNums(numStr) {
     }
     return nums;
 }
+
+function getMean(nums) {
+    let total = 0;
+
+    nums.forEach(num => {
+        total += num;
+    });
+    return total / nums.length;
+}
+
+function getMedian(nums) {
+    nums.sort((a, b) =. (a - b));
+
+    if (nums.length === 1) {
+        return nums[0];
+    }
+    if (nums.length % 2 === 1) {
+        return nums[Math.floor(nums.length / 2)];
+    } else {
+        return (nums[nums.length / 2] + nums[nums.length / 2 -1]) / 2;
+    }
+}
+
+function getMode(nums) {
+
+    let numSet = new Set(nums);
+
+    let numCounts = {};
+
+    numSet.forEach(num => {
+        numCounts[num] = 0;
+        index = nums.indexOf(num , 0);
+        while (index !== -1){
+            index ++;
+            numCounts[num] ++;
+
+            index = nums.indexOf(num , index);
+        }
+    });
+
+    let max = 2;
+    let modes = [];
+
+    for (num in numCounts){
+        if (numCounts[num] > max){
+            modes = [Number(num)];
+
+            max = numCounts[num];
+        } else if (numCounts[num] === max){
+            modes.push(Number(num));
+        }
+    }
+
+    return modes;
+}
+
+
+module.exports = {
+    getNums: getNums,
+    getMean: getMean,
+    getMedian: getMedian,
+    getMode: getMode
 
