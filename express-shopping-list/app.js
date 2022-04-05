159ed77f437fd065594629c7776ebd194734a4d1
jen f <77820429+jenfausto@users.noreply.github.com>
2022-04-04 19:16:03 -0600

    app.js and expressError.js created

---
+++ express-shopping-list/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const expressError = require("./expressError")
+
+app.use(express.json());
+
+// 404 handler
+app.use(function (req, res, next) {
+    const notFoundError = new ExpressError("Not Found", 404);
+    return next(notFoundError)
+  });  
+
+/** general error handler */
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+  
+    return res.json({
+      error: err.message,
+    });
+  });
+  
+  module.exports = app;
