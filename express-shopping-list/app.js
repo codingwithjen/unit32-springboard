7e8181676a20280c63b97c72bac38ea4eb2b8ba5
jen f <77820429+jenfausto@users.noreply.github.com>
2022-04-04 21:21:49 -0600

    finished

--- express-shopping-list/app.js
+++ express-shopping-list/app.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app = express()
-const expressError = require("./expressError")
+const express = require('express');
+const app = express();
+const itemsRoutes = require('./routes/items')
+const ExpressError = require("./expressError");
 
 app.use(express.json());
+app.use("/items", itemsRoutes);
 
 // 404 handler
 app.use(function (req, res, next) {
@@ -12,11 +14,14 @@ app.use(function (req, res, next) {
 
 /** general error handler */
 
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
+app.use(function(err, req, res, next) {
+    // the default status is 500 Internal Server Error
+    let status = err.status || 500;
+    let message = err.message;
   
-    return res.json({
-      error: err.message,
+    // set the status and alert the user
+    return res.status(status).json({
+      error: {message, status}
     });
   });
   
